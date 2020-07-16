11877761a6be29f50600e3ccc1376045f48812d4
mebooker <45566410+mebooker@users.noreply.github.com>
2020-07-16 17:57:08 -0400

    Add files via upload

--- examples/demo/env_interactive_example.py
+++ examples/demo/env_interactive_example.py
@@ -1,4 +1,4 @@
-from gibson2.envs.locomotor_env import NavigateRandomEnv
+from gibson2.envs.locomotor_env import NavigateRandomInitEnvSim2Real
 from time import time
 import numpy as np
 from time import time
@@ -9,8 +9,8 @@ from gibson2.core.render.profiler import Profiler
 
 def main():
     config_filename = os.path.join(os.path.dirname(gibson2.__file__),
-                                   '../examples/configs/turtlebot_interactive_demo.yaml')
-    nav_env = NavigateRandomEnv(config_file=config_filename, mode='gui')
+                                   '../examples/configs/turtlebot_AMR.yaml')
+    nav_env = NavigateRandomInitEnvSim2Real(config_file=config_filename, mode='gui', track='interactive', random_init_m=0)
     for j in range(10):
         nav_env.reset()
         for i in range(100):
